496a2c40bbd08442ce2c599f283cce4682ad90fe
rory619 <118372080+rory619@users.noreply.github.com>
2025-12-31 13:28:22 +0000

    Changed testing files to get test coverage above 80%

--- app/schemas.py
+++ app/schemas.py
@@ -1,23 +1,19 @@
-from typing import Annotated, Optional, List 
-from annotated_types import Ge, Le             
-from pydantic import BaseModel, EmailStr, ConfigDict, StringConstraints 
- 
-# ---------- Reusable type aliases ---------- 
+from typing import Annotated
+from annotated_types import Ge, Le
+from pydantic import BaseModel, ConfigDict, StringConstraints
+
+# ---------- Reusable type aliases ----------
 CodeStr = Annotated[str, StringConstraints(min_length=1, max_length=32)]
 CourseNameStr = Annotated[str, StringConstraints(min_length=1, max_length=255)]
 CreditsInt = Annotated[int, Ge(1), Le(120)]
- 
 
- 
-class CourseCreate(BaseModel): 
-   code: CodeStr
-   name: CourseNameStr
-   credits: CreditsInt
 
-class CourseRead(BaseModel): 
-   id: int
+class CourseCreate(BaseModel):
+    code: CodeStr
+    name: CourseNameStr
+    credits: CreditsInt
 
-   
-   model_config = ConfigDict(from_attributes=True) 
-  
-    
+
+class CourseRead(CourseCreate):
+    id: int
+    model_config = ConfigDict(from_attributes=True)
